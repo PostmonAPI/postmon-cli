cbb1d3a314a88945a92bc0b24f718f4c9463c978
Breno Moura <basmoura@gmail.com>
2014-05-05 14:35:32 -0300

    Transforma as opcoes parâmetros posicionais para o que é obrigatório.

--- lib/postmon_ruby/gem_output.rb
+++ lib/postmon_ruby/gem_output.rb
@@ -1,23 +1,5 @@
 module PostmonRuby
   class GemOutput
-    def self.get_info(options)
-      unless options[:cep].nil?
-        cep(options[:cep])
-      end
-
-      unless options[:cidade].nil?
-        cidade(options[:cidade].first, options[:cidade].last)
-      end
-
-      unless options[:estado].nil?
-        estado(options[:estado])
-      end
-
-      unless options[:rastreio].nil?
-        rastreio(options[:rastreio])
-      end      
-    end
-
     private
     def self.cep(cep)
       resultado = Client.search :cep, cep
@@ -30,14 +12,14 @@ module PostmonRuby
     end
 
     def self.estado(uf)
-      resultado = PostmonRuby::Client.search :estado, uf
+      resultado = Client.search :estado, uf
       output_variables(resultado)
     end
 
     def self.rastreio(rastreio)
-      resultado = PostmonRuby::Client.search :rastreio, :ect, rastreio
+      resultado = Client.search :rastreio, :ect, rastreio
       output_variables(resultado)
-    end    
+    end
 
     def self.output_variables(resultado)
       if resultado.not_found
@@ -61,7 +43,7 @@ module PostmonRuby
         else
           puts "#{k}: #{v}"
         end
-      end      
+      end
     end
   end
 end
