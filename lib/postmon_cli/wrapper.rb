9fce5912d12af99e14acba469a0c0a78fddffe86
Breno Moura <basmoura@gmail.com>
2014-05-11 14:40:15 -0300

    Altera comandos para chamar os metodos da class output

--- lib/postmon_cli/wrapper.rb
+++ lib/postmon_cli/wrapper.rb
@@ -2,20 +2,24 @@ require 'postmon_ruby'
 
 module PostmonCli
   class Wrapper
-    def self.get_cep(cep)
-      PostmonRuby::GemOutput.cep(cep)
+    def self.cep(cep)
+      resultado = PostmonRuby::Client.search :cep, cep
+      Output.resultado(resultado)
     end
 
-    def self.get_cidade(uf, cidade)
-      PostmonRuby::GemOutput.cidade(uf, cidade)
+    def self.cidade(uf, cidade)
+      resultado = PostmonRuby::Client.search :cidade, uf, cidade
+      Output.resultado(resultado)
     end
 
-    def self.get_estado(uf)
-      PostmonRuby::GemOutput.estado(uf)
+    def self.estado(uf)
+      resultado = PostmonRuby::Client.search :estado, uf
+      Output.resultado(resultado)
     end
 
-    def self.get_rastreio(codigo)
-      PostmonRuby::GemOutput.rastreio(codigo)
+    def self.rastreio(rastreio)
+      resultado = PostmonRuby::Client.search :rastreio, :ect, rastreio
+      Output.resultado(resultado)
     end
   end
 end
